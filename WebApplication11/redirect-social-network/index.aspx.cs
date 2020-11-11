d232132c152fea253c0a6e2b78b0206449b156b9
Bastien Languedoc <bastien.languedoc@outlook.fr>
2020-11-11 21:27:17 +0100

    update 11/11/20

---
+++ WebApplication11/redirect-social-network/index.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Renci.SshNet;
+
+namespace WebApplication11.redirect_social_network
+{
+    public partial class index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+
+            string v = Request.QueryString["url"];
+            if (v != "")
+                Response.Redirect("/404");
+         
+            else
+            {
+                if (v != "https://twitter.com/synhostinger_fr")
+                    Response.Redirect(v);
+
+             else
+            {
+                if (v != "https://www.facebook.com/synhostinger/")
+                    Response.Redirect("/");
+            }
+
+        }
+    }
+   }
+}
